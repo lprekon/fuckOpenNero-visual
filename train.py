8ea667150af345c546770d54017ecb059f797faa
John O'kane <jtommyok8@gmail.com>
2017-11-19 23:17:31 -0500

    some helper functions for training

---
+++ train.py
@@ -0,0 +1,30 @@
+from classifier import *
+
+def main():
+	buddy = ObjectClassifier()
+	buddy.train()
+	print(buddy.learned_model)
+
+if __name__ == '__main__':
+	main()
+
+def find_average_feature():
+	buddy = ObjectClassifier()
+	values = {}
+	for f in buddy.features.keys():
+		values[f] = []
+	for classification in buddy.labels:
+		data_folder = "./snapshots/training_data/" + classification + "/"
+		images = [(data_folder + f) for f in listdir(data_folder) if isfile(join(data_folder, f))]
+		for image in images:
+			image_data = load_image(image)  # <-- this takes ~ .75 seconds. Slows everything down
+			for feature_func in buddy.features.keys():
+				values[feature_func].append(feature_func(image_data))
+	for feature in buddy.features.keys():
+		print(feature)
+		data = values[feature]
+		mean = sum(data) / len(data)
+		median = data[len(data) // 2]
+		print("\tmean: " + str(mean))
+		print("\tmeadian: " + str(median))
+                    
